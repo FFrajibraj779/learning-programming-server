f9cab107050983ab95dc30570181dedab9a81a09
Md Rajikul Islame <mdrajikul779@gmail.com>
2022-10-25 19:21:11 +0600

    data loaded

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const app = express()
 const port = process.env.PORT || 5000 ;
+const cors = require("cors");
 
+app.use(cors())
+
+const category = require('./category.json')
 
 app.get('/', (req, res)=>{
     res.send('assignment sevar')
 })
+
+app.get('/category', (req, res)=>{
+    res.send(category);
+})
+ app.get('/category/:id', (req, res)=>{
+    const id= req.params.id;
+    const data = category.filter(d=>d.id==id)
+  
+    res.send(data)
+ })
 
 app.listen(port, ()=>{
     console.log(`server running on port ${port}`);
